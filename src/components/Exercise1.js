657188a9e94ad1e78e73de7c6327b3d551a48642
Or <orlevy11@gmail.com>
2022-11-09 12:54:54 +0200

    Ex 1

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -1,24 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Exercise1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      age: "",
+    };
+  }
 
-    constructor() {
-        super()
-        this.state = {
-            name: "",
-            age: ""
-        }
-    }
+  alertInputs = () => {
+    alert(
+      this.state.name !== "" || this.state.age !== ""
+        ? `Hello ${this.state.name}, you are ${this.state.age}`
+        : "No inputs to show"
+    );
+  };
 
-    render() {
-        return (
-            <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
-            </div>
-        );
-    }
+  updateInputs = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          id="name-input"
+          name="name"
+          value={this.state.name}
+          onChange={this.updateInputs}
+        />
+        <input
+          id="age-input"
+          name="age"
+          value={this.state.age}
+          onChange={this.updateInputs}
+        />
+        <button onClick={this.alertInputs}>Go to Bar</button>
+      </div>
+    );
+  }
 }
 
 export default Exercise1;
